5e032242a931f12bdee9a8c0eff4e3554ec1f8fe
AliAksoyy <alifeyza4017@gmail.com>
2023-07-20 12:19:08 +0300

    added interface and public

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
 export {}
 console.log("hello typescript")
 
-interface Person {
-  firstName:string,
-  lastName:string
+
+interface IPerson {
+  name:string
 }
 
-interface Employee {
+interface IEmployee {
   id:number,
-  salary:number
-  department:string
+  salary:number,
+  depart:string
 }
 
+class Instructor implements IPerson, IEmployee {
+  name:string;
+
+  constructor(name:string, public id:number, public salary:number, public depart:string){
+    this.name=name
+  }
+}
+
+let ali = new Instructor("ali", 1,15,"front")
+console.log(ali)
+
+function calis(value:IEmployee):void{
+  console.log(value.id + " " + value.salary + " " + value.depart )
+}
+
+calis({id:3, salary:3,depart:"2"})
