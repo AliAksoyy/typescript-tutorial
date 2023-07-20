e35df405ad54811d49fc4f1497945fdd6208422e
AliAksoyy <alifeyza4017@gmail.com>
2023-07-20 11:48:16 +0300

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
 export {}
 console.log("hello typescript")
 
+interface Person {
+  firstName:string,
+  lastName:string
+}
+
+interface Employee {
+  id:number,
+  salary:number
+  department:string
+}
+
