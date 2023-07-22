a9fccc6f0e1fc52bfb4ceca408083bf2b1c7f37d
AliAksoyy <alifeyza4017@gmail.com>
2023-07-22 16:50:52 +0300

    abstract class

--- typeScript-SecondDay/main.ts
+++ typeScript-SecondDay/main.ts
@@ -1,15 +1,1 @@
-export {};
-
-abstract class Database {
-  get() {
-    console.log("get");
-  }
-  add() {
-    console.log("add");
-  }
-
-  abstract delete();
-  abstract update();
-}
-
-
+export {}
