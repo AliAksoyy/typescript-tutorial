add5206b88875621a9b13134fb9100de47846b07
AliAksoyy <alifeyza4017@gmail.com>
2023-07-22 14:12:27 +0300

    add

--- typeScript-SecondDay/IproductService.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from "./Products";
-
-export interface IProductService {
-  getById(id: number): Product;
-  getProducts(): Product[];
-  saveProduct(product: Product): void;
-  deleteProduct(product: Product): void;
-}
