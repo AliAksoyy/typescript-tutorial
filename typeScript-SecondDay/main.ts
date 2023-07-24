c0161f6240d0fa6db85b547e8bfd010a67558676
AliAksoyy <alifeyza4017@gmail.com>
2023-07-24 16:54:46 +0300

    add

--- typeScript-SecondDay/main.ts
+++ typeScript-SecondDay/main.ts
@@ -1,39 +1,39 @@
 // **Generic Examples *************************************1**********************************
 
-class Store<T> {
-  private items: T[];
+// class Store<T> {
+//   private items: T[];
 
-  constructor() {
-    this.items = [];
-  }
+//   constructor() {
+//     this.items = [];
+//   }
 
-  addItem(item: T) {
-    this.items.push(item);
-  }
+//   addItem(item: T) {
+//     this.items.push(item);
+//   }
 
-  getItems(): T[] {
-    return this.items;
-  }
-}
+//   getItems(): T[] {
+//     return this.items;
+//   }
+// }
 
-class Product {
-  name: string;
-  price: number;
+// class Product {
+//   name: string;
+//   price: number;
 
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-}
+//   constructor(name: string, price: number) {
+//     this.name = name;
+//     this.price = price;
+//   }
+// }
 
-class Ali extends Store<Product> {}
+// class Ali extends Store<Product> {}
 
-const aliStore = new Ali();
-aliStore.addItem(new Product("Laptop", 1000));
-aliStore.addItem(new Product("Phone", 500));
+// const aliStore = new Ali();
+// aliStore.addItem(new Product("Laptop", 1000));
+// aliStore.addItem(new Product("Phone", 500));
 
-const products = aliStore.getItems();
-console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
+// const products = aliStore.getItems();
+// console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
 
 // **Generic Examples *************************************2**********************************
 
@@ -53,3 +53,26 @@ console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
 // let product1: Product = { name: "Laptop", price: 23 };
 
 // console.log(update(product1, "name", "7"));
+
+// **Generic Examples *************************************3**********************************
+
+// const arr1: Array<string> = ["name", "price", "id"];
+
+// const arr2 = [1, 2, 3];
+
+// const x = arr1.pop();
+// const y = arr2.pop();
+
+// const p1 = Promise.resolve("hello");
+// const p2 = Promise.resolve(3.14);
+
+// (async () => {
+//   const x = await p1;
+//   const y = await p2;
+// })();
+
+type JobRun = {
+  job: any;
+  state: "queued" | "running" | "completed";
+  onComplete: (cb: (job: any) => void) => void;
+};
