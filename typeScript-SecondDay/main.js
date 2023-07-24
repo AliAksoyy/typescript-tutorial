92bf08b048bf0fd3e6903b3587f563cd8ab2bdea
AliAksoyy <alifeyza4017@gmail.com>
2023-07-24 16:12:59 +0300

    add function generic

--- typeScript-SecondDay/main.js
+++ typeScript-SecondDay/main.js
@@ -1,47 +1,18 @@
-// **Generic **
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+// **Generic Examples *************************************1**********************************
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
     };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Store = /** @class */ (function () {
-    function Store() {
-        this.items = [];
-    }
-    Store.prototype.addItem = function (item) {
-        this.items.push(item);
-    };
-    Store.prototype.getItems = function () {
-        return this.items;
-    };
-    return Store;
-}());
-var Product = /** @class */ (function () {
-    function Product(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-    return Product;
-}());
-var Ali = /** @class */ (function (_super) {
-    __extends(Ali, _super);
-    function Ali() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return Ali;
-}(Store));
-var aliStore = new Ali();
-aliStore.addItem(new Product("Laptop", 1000));
-aliStore.addItem(new Product("Phone", 500));
-var products = aliStore.getItems();
-console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
+    return __assign.apply(this, arguments);
+};
+function update(obj, key, newValue) {
+    var _a;
+    return __assign(__assign({}, obj), (_a = {}, _a[key] = newValue, _a));
+}
+var product1 = { name: "Laptop", price: 23 };
+console.log(update(product1, "name", "7"));
--- typeScript-SecondDay/main.ts
+++ typeScript-SecondDay/main.ts
@@ -1,36 +1,55 @@
-// **Generic **
+// **Generic Examples *************************************1**********************************
 
-class Store<T> {
-  private items: T[];
+// class Store<T> {
+//   private items: T[];
 
-  constructor() {
-    this.items = [];
-  }
+//   constructor() {
+//     this.items = [];
+//   }
 
-  addItem(item: T) {
-    this.items.push(item);
-  }
+//   addItem(item: T) {
+//     this.items.push(item);
+//   }
 
-  getItems(): T[] {
-    return this.items;
-  }
-}
+//   getItems(): T[] {
+//     return this.items;
+//   }
+// }
 
-class Product {
+// class Product {
+//   name: string;
+//   price: number;
+
+//   constructor(name: string, price: number) {
+//     this.name = name;
+//     this.price = price;
+//   }
+// }
+
+// class Ali extends Store<Product> {}
+
+// const aliStore = new Ali();
+// aliStore.addItem(new Product("Laptop", 1000));
+// aliStore.addItem(new Product("Phone", 500));
+
+// const products = aliStore.getItems();
+// console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
+
+// **Generic Examples *************************************2**********************************
+
+interface Product {
   name: string;
   price: number;
-
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
 }
 
-class Ali extends Store<Product> {}
+function update<T extends object, K extends keyof T>(
+  obj: T,
+  key: K,
+  newValue: T[K]
+): T {
+  return { ...obj, [key]: newValue };
+}
 
-const aliStore = new Ali();
-aliStore.addItem(new Product("Laptop", 1000));
-aliStore.addItem(new Product("Phone", 500));
+let product1: Product = { name: "Laptop", price: 23 };
 
-const products = aliStore.getItems();
-console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
+console.log(update(product1, "name", "7"));
