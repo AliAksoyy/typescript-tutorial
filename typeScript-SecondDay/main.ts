d83a81d5d43f50b0e831562e4ad96951ddb7d030
AliAksoyy <alifeyza4017@gmail.com>
2023-07-24 16:45:33 +0300

    add

--- typeScript-SecondDay/main.ts
+++ typeScript-SecondDay/main.ts
@@ -1,55 +1,55 @@
 // **Generic Examples *************************************1**********************************
 
-// class Store<T> {
-//   private items: T[];
+class Store<T> {
+  private items: T[];
 
-//   constructor() {
-//     this.items = [];
-//   }
+  constructor() {
+    this.items = [];
+  }
 
-//   addItem(item: T) {
-//     this.items.push(item);
-//   }
+  addItem(item: T) {
+    this.items.push(item);
+  }
 
-//   getItems(): T[] {
-//     return this.items;
-//   }
-// }
+  getItems(): T[] {
+    return this.items;
+  }
+}
 
-// class Product {
-//   name: string;
-//   price: number;
+class Product {
+  name: string;
+  price: number;
 
-//   constructor(name: string, price: number) {
-//     this.name = name;
-//     this.price = price;
-//   }
-// }
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
 
-// class Ali extends Store<Product> {}
+class Ali extends Store<Product> {}
 
-// const aliStore = new Ali();
-// aliStore.addItem(new Product("Laptop", 1000));
-// aliStore.addItem(new Product("Phone", 500));
+const aliStore = new Ali();
+aliStore.addItem(new Product("Laptop", 1000));
+aliStore.addItem(new Product("Phone", 500));
 
-// const products = aliStore.getItems();
-// console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
+const products = aliStore.getItems();
+console.log(products); // Output: [Product { name: 'Laptop', price: 1000 },
 
 // **Generic Examples *************************************2**********************************
 
-interface Product {
-  name: string;
-  price: number;
-}
+// interface Product {
+//   name: string;
+//   price: number;
+// }
 
-function update<T extends object, K extends keyof T>(
-  obj: T,
-  key: K,
-  newValue: T[K]
-): T {
-  return { ...obj, [key]: newValue };
-}
+// function update<T extends object, K extends keyof T>(
+//   obj: T,
+//   key: K,
+//   newValue: T[K]
+// ): T {
+//   return { ...obj, [key]: newValue };
+// }
 
-let product1: Product = { name: "Laptop", price: 23 };
+// let product1: Product = { name: "Laptop", price: 23 };
 
-console.log(update(product1, "name", "7"));
+// console.log(update(product1, "name", "7"));
