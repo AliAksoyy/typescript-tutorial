d0ce6531ae0e63f2c06ff7ad73c5e2fd0d66ae1d
AliAksoyy <alifeyza4017@gmail.com>
2023-07-24 17:44:03 +0300

    add

--- typeScript-SecondDay/main.js
+++ typeScript-SecondDay/main.js
@@ -1,1 +1,21 @@
 // **Generic Examples *************************************1**********************************
+function enqueueJob(job) {
+    job.name;
+    return {
+        job: job,
+        state: "queued",
+        onComplete: function (cb) { return cb(job); },
+    };
+}
+var j = {
+    recipentEmail: "alice@gmail",
+    subject: "hii",
+    name: "send-email",
+    start: function () { return null; },
+    state: "inComplete",
+};
+var run = enqueueJob(j);
+console.log(run);
+run.onComplete(function (job) {
+    console.log(job);
+});
--- typeScript-SecondDay/main.ts
+++ typeScript-SecondDay/main.ts
@@ -71,8 +71,44 @@
 //   const y = await p2;
 // })();
 
-type JobRun = {
-  job: any;
-  state: "queued" | "running" | "completed";
-  onComplete: (cb: (job: any) => void) => void;
+type Job = {
+  name: string;
+  start: () => void;
+  state: "inComplete" | "success" | "failure";
 };
+
+type JobRun<T extends Job> = {
+  job: T;
+  state: "queued" | "running" | "completed";
+  onComplete: (cb: (job: T) => void) => void;
+};
+
+type SendEmailJob = Job & {
+  recipentEmail: string;
+  subject: string;
+};
+
+function enqueueJob<T extends Job>(job: T): JobRun<typeof job> {
+  job.name;
+  return {
+    job,
+    state: "queued",
+    onComplete: (cb: (job: T) => void) => cb(job),
+  };
+}
+
+const j: SendEmailJob = {
+  recipentEmail: "alice@gmail",
+  subject: "hii",
+  name: "send-email",
+  start: () => null,
+  state: "inComplete",
+};
+
+const run = enqueueJob(j);
+
+console.log(run);
+
+run.onComplete((job) => {
+  console.log(job);
+});
