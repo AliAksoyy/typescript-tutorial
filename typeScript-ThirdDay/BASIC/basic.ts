94939aced22b86303276de65f4489250ea51b467
AliAksoyy <alifeyza4017@gmail.com>
2023-07-26 14:27:03 +0300

    type Aliases

--- typeScript-ThirdDay/BASIC/basic.ts
+++ typeScript-ThirdDay/BASIC/basic.ts
@@ -10,4 +10,25 @@ function createCourse(): { name: string; price: number } {
   return { name: "reactjs", price: 300 };
 }
 
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
 
+function createUserOne(user: User): User {
+  return user;
+}
+
+createUserOne({ name: "ali", email: "ali@gmail.com", isActive: false });
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+function printCoord(coor: Point): void {
+  console.log(coor.x, coor.y);
+}
+
+printCoord({ x: 2, y: 1 });
