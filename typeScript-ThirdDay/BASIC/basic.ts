34dca184003aa2a7f8eee1e3e85cd84468fd8cfb
AliAksoyy <alifeyza4017@gmail.com>
2023-07-26 14:20:26 +0300

    type Alias

---
+++ typeScript-ThirdDay/BASIC/basic.ts
@@ -0,0 +1,13 @@
+export {};
+
+const User = {
+  name: "ali",
+  email: "ali@gmail.com",
+  isActive: true,
+};
+
+function createCourse(): { name: string; price: number } {
+  return { name: "reactjs", price: 300 };
+}
+
+
