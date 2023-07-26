e75324ae6d013dc8ca87f68569ab355b2f47f1f1
AliAksoyy <alifeyza4017@gmail.com>
2023-07-26 11:29:16 +0300

    typescript types

--- typeScript-ThirdDay/INTRO/intro.ts
+++ typeScript-ThirdDay/INTRO/intro.ts
@@ -6,3 +6,22 @@ console.log("ali");
 let greetings: string = "Hello Ali";
 
 console.log(greetings);
+
+// number
+
+let userId: number = 334455.4;
+
+let id: number;
+id = 44.5;
+id.toFixed();
+console.log(id);
+
+let isLog: boolean = false;
+
+let hero;
+
+function getHero() {
+  return "thor";
+}
+hero = getHero(); // hero type any
+
