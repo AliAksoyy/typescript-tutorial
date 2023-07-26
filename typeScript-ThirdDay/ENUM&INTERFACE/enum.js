64b6f7fcb21fe7bd5d5a86c959ae7107b7f5ba9b
AliAksoyy <alifeyza4017@gmail.com>
2023-07-26 15:49:09 +0300

    enum finished

---
+++ typeScript-ThirdDay/ENUM&INTERFACE/enum.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ali = ["1", 2];
+console.log(ali);
+ali.push(3);
+console.log("object", ali);
---
+++ typeScript-ThirdDay/ENUM&INTERFACE/enum.ts
@@ -0,0 +1,17 @@
+export {};
+// let seat;
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+
+// if (seat == 0) {
+// }
+
+enum SeatChoice {
+  AISLE = "Aisle",
+  MIDDLE = 6,
+  WINDOW,
+  FOURTH,
+}
+
+const hcSeat = SeatChoice.AISLE;
