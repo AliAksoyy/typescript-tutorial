44a8d02781ba2922e0a2377ba805b1a625a2e8e2
AliAksoyy <alifeyza4017@gmail.com>
2023-07-27 10:31:54 +0300

    getter setter

--- typeScript-ThirdDay/CLASS/dist/index.js
+++ typeScript-ThirdDay/CLASS/dist/index.js
@@ -1,11 +1,40 @@
 "use strict";
 class User {
     constructor(email, name) {
+        this._courseCount = 1;
         this.city = "kirikkale";
         this.email = email;
         this.name = name;
     }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
 }
 const ali = new User("ali@gmail.com", "ali");
 // ali.city = "2"; //?? Cannot assign to 'city' because it is a read-only property
-console.log(ali);
+// console.log(ali);
+// ali.name //?? Property 'name' is private and only accessible within class 'User'
+// ali.deleteToken() // ?? Property 'deleteToken' is private and only accessible within class 'User'.ts(2341)
+console.log(ali.courseCount);
+//?? buda kısa yol
+class User1 {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "kirikkale";
+    }
+}
+let ali1 = new User1("ali@", "ali1");
+// console.log(ali1);
--- typeScript-ThirdDay/CLASS/src/index.ts
+++ typeScript-ThirdDay/CLASS/src/index.ts
@@ -1,14 +1,47 @@
 class User {
-  email: string;
-  name: string;
+  private _courseCount = 1;
+  public email: string;
+  private name: string;
   readonly city: string = "kirikkale";
 
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
   }
+  private deleteToken() {
+    console.log("token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const ali = new User("ali@gmail.com", "ali");
 // ali.city = "2"; //?? Cannot assign to 'city' because it is a read-only property
-console.log(ali);
+// console.log(ali);
+// ali.name //?? Property 'name' is private and only accessible within class 'User'
+
+// ali.deleteToken() // ?? Property 'deleteToken' is private and only accessible within class 'User'.ts(2341)
+
+console.log(ali.courseCount);
+
+
+//?? buda kısa yol
+class User1 {
+  readonly city: string = "kirikkale";
+  constructor(public email: string, public name: string) {}
+}
+let ali1 = new User1("ali@", "ali1");
+// console.log(ali1);
