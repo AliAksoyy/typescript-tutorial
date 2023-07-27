2e1e1bfd148a988dbb936e17e0158625edf021c5
AliAksoyy <alifeyza4017@gmail.com>
2023-07-27 12:23:23 +0300

    genercic class started

--- typeScript-ThirdDay/GENERICS/dist/index.js
+++ typeScript-ThirdDay/GENERICS/dist/index.js
@@ -1,3 +1,26 @@
 "use strict";
-console.log("object");
-console.log("object")
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function idetntityThree(val) {
+    return val;
+}
+function identityFour(values) {
+    return values[0];
+}
+// identityFour< Bootle>({brand:"2",})
+function getSearchProducts(products) {
+    //do some database operations
+    const myIndex = 3;
+    return products[myIndex];
+}
+const getModeSearchProducts = (products) => {
+    // do some database operations
+    const myIndex = 2;
+    return products[myIndex];
+};
--- typeScript-ThirdDay/GENERICS/src/index.ts
+++ typeScript-ThirdDay/GENERICS/src/index.ts
@@ -1,1 +1,36 @@
-console.log("object")
+const score: Array<number> = [];
+const names: Array<string> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+}
+function identityTwo(val: any): any {
+  return val;
+}
+
+function idetntityThree<Type>(val: Type): Type {
+  return val;
+}
+
+function identityFour<T>(values: T[]): T {
+  return values[0];
+}
+
+interface Bootle {
+  brand: string;
+  type: number;
+}
+
+// identityFour< Bootle>({brand:"2",})
+
+function getSearchProducts<T>(products: T[]): T {
+  //do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getModeSearchProducts = <T>(products: T[]): T => {
+  // do some database operations
+  const myIndex = 2;
+  return products[myIndex];
+};
