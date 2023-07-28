69c731092ce335402f32b0707d70cc2703efdbae
AliAksoyy <alifeyza4017@gmail.com>
2023-07-28 17:36:53 +0300

    finished array

--- typeScript-FourthDay/LESSON03/build/js/main.js
+++ typeScript-FourthDay/LESSON03/build/js/main.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("object");
+let stringArray = ["ali", "beyda"];
+let guitars = ["Strat", "Les"];
+let arrayData1 = [1, 2, "ali", "beyda"];
+let arrayData2 = [false, 2];
+let mixedData = ["strat", 3, false];
+let arrayData = ["ali", 3, false];
+// arrayData2 = guitars; //?? Type 'string' is not assignable to type 'number | boolean'
+let test = [];
+let bands = [];
+bands.push("Ali Aksoy");
+//Tuple
+let myTuple = ["Ali Aksoy", 2, false, 2];
+myTuple.push("asd");
+// Object
+let myObj;
+myObj = [];
+console.log(typeof myObj);
--- typeScript-FourthDay/LESSON03/src/main.ts
+++ typeScript-FourthDay/LESSON03/src/main.ts
@@ -1,1 +1,30 @@
-console.log("object")
+let stringArray: string[] = ["ali", "beyda"];
+
+let guitars: Array<string> = ["Strat", "Les"];
+
+let arrayData1: (string | number)[] = [1, 2, "ali", "beyda"];
+
+let arrayData2: Array<boolean | number> = [false, 2];
+
+let mixedData: Array<any> = ["strat", 3, false];
+
+let arrayData: [string, number, boolean] = ["ali", 3, false];
+
+// arrayData2 = guitars; //?? Type 'string' is not assignable to type 'number | boolean'
+
+let test = [];
+
+let bands: string[] = [];
+
+bands.push("Ali Aksoy");
+
+//Tuple
+
+let myTuple: [string, number, boolean, number] = ["Ali Aksoy", 2, false, 2];
+myTuple.push("asd");
+
+// Object
+
+let myObj: object;
+myObj = [];
+console.log(typeof myObj)
