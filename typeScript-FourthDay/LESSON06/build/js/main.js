b76cf87e947fe184689e2000b4369ccab968571f
AliAksoyy <alifeyza4017@gmail.com>
2023-07-31 14:56:36 +0300

    added

--- typeScript-FourthDay/LESSON06/build/js/main.js
+++ typeScript-FourthDay/LESSON06/build/js/main.js
@@ -1,87 +1,70 @@
 "use strict";
-// class Coder {
-//   name: string;
-//   music: string;
-//   age: number;
-//   lang: String;
-//   constructor(name: string, music: string, age: number, lang: string) {
-//     this.name = name;
-//     this.music = music;
-//     this.age = age;
-//     this.lang = lang;
-//   }
-// }
-// class Coder1 {
-//   secondLang!: string;
-//   constructor(
-//     public readonly name: string,
-//     public music: string,
-//     private age: number,
-//     protected lang: string = "typescript"
-//   ) {
-//     this.name = name;
-//     this.music = music;
-//     this.age = age;
-//     this.lang = lang;
-//   }
-//   public getAge() {
-//     return `Hello I'm ${this.age}`;
-//   }
-// }
-// let ali = new Coder1("ali", "neset", 3, "turkish");
-// console.log(ali);
-// console.log(ali.getAge());
-// // console.log(ali.age); //?Property 'age' is private and only accessible within class 'Coder1'
-// // console.log(ali.lang); // ?? Property 'lang' is protected and only accessible within class 'Coder1' and its subclasses
-// class WebDev extends Coder1 {
-//   constructor(
-//     public computer: string,
-//     name: string,
-//     music: string,
-//     age: number
-//   ) {
-//     super(name, music, age);
-//     this.computer = computer;
-//   }
-//   public getLang() {
-//     return `I write ${this.lang}`;
-//   }
-// }
-// const Sara = new WebDev("Mac","Sarah","Lofi",25);
-// console.log(Sara)
-// interface Musician {
-//   name: string;
-//   instrument: string;
-//   play(action: string): string;
-// }
-// class Guitarist implements Musician {
-//   name: string;
-//   instrument: string;
-//   constructor(name: string, instrument: string) {
-//     this.name = name;
-//     this.instrument = instrument;
-//   }
-//   play(action: string): string {
-//     return `${this.name} ${action}`;
-//   }
-// }
-// let Page = new Guitarist("ali", "guitar");
-// console.log(Page.play("strums"));
-// class Peeps {
-//   static count: number = 0;
-//   static getCount(): number {
-//     return Peeps.count;
-//   }
-//   public id: number;
-//   constructor(public name: string) {
-//     this.name = name;
-//     this.id = ++Peeps.count;
-//   }
-// }
-// const beyda = new Peeps("beyda");
-// const feyza = new Peeps("feyza");
-// console.log(Peeps.count);
-// console.log(feyza.id);
+class Coder {
+    constructor(name, music, age, lang) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+}
+class Coder1 {
+    constructor(name, music, age, lang = "typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello I'm ${this.age}`;
+    }
+}
+let ali = new Coder1("ali", "neset", 3, "turkish");
+console.log(ali);
+console.log(ali.getAge());
+// console.log(ali.age); //?Property 'age' is private and only accessible within class 'Coder1'
+// console.log(ali.lang); // ?? Property 'lang' is protected and only accessible within class 'Coder1' and its subclasses
+class WebDev extends Coder1 {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev("Mac", "Sarah", "Lofi", 25);
+console.log(Sara);
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action}`;
+    }
+}
+let Page = new Guitarist("ali", "guitar");
+console.log(Page.play("strums"));
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const beyda = new Peeps("beyda");
+const feyza = new Peeps("feyza");
+console.log(Peeps.count);
+console.log(feyza.id);
 class Bands {
     constructor() {
         this.dataState = [];
--- typeScript-FourthDay/LESSON06/src/main.ts
+++ typeScript-FourthDay/LESSON06/src/main.ts
@@ -1,104 +1,104 @@
-// class Coder {
-//   name: string;
-//   music: string;
-//   age: number;
-//   lang: String;
+class Coder {
+  name: string;
+  music: string;
+  age: number;
+  lang: String;
 
-//   constructor(name: string, music: string, age: number, lang: string) {
-//     this.name = name;
-//     this.music = music;
-//     this.age = age;
-//     this.lang = lang;
-//   }
-// }
-// class Coder1 {
-//   secondLang!: string;
+  constructor(name: string, music: string, age: number, lang: string) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
+}
+class Coder1 {
+  secondLang!: string;
 
-//   constructor(
-//     public readonly name: string,
-//     public music: string,
-//     private age: number,
-//     protected lang: string = "typescript"
-//   ) {
-//     this.name = name;
-//     this.music = music;
-//     this.age = age;
-//     this.lang = lang;
-//   }
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "typescript"
+  ) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
 
-//   public getAge() {
-//     return `Hello I'm ${this.age}`;
-//   }
-// }
+  public getAge() {
+    return `Hello I'm ${this.age}`;
+  }
+}
 
-// let ali = new Coder1("ali", "neset", 3, "turkish");
-// console.log(ali);
-// console.log(ali.getAge());
-// // console.log(ali.age); //?Property 'age' is private and only accessible within class 'Coder1'
-// // console.log(ali.lang); // ?? Property 'lang' is protected and only accessible within class 'Coder1' and its subclasses
+let ali = new Coder1("ali", "neset", 3, "turkish");
+console.log(ali);
+console.log(ali.getAge());
+// console.log(ali.age); //?Property 'age' is private and only accessible within class 'Coder1'
+// console.log(ali.lang); // ?? Property 'lang' is protected and only accessible within class 'Coder1' and its subclasses
 
-// class WebDev extends Coder1 {
-//   constructor(
-//     public computer: string,
-//     name: string,
-//     music: string,
-//     age: number
-//   ) {
-//     super(name, music, age);
-//     this.computer = computer;
-//   }
+class WebDev extends Coder1 {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
 
-//   public getLang() {
-//     return `I write ${this.lang}`;
-//   }
-// }
+  public getLang() {
+    return `I write ${this.lang}`;
+  }
+}
 
-// const Sara = new WebDev("Mac","Sarah","Lofi",25);
-// console.log(Sara)
+const Sara = new WebDev("Mac","Sarah","Lofi",25);
+console.log(Sara)
 
-// interface Musician {
-//   name: string;
-//   instrument: string;
-//   play(action: string): string;
-// }
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
 
-// class Guitarist implements Musician {
-//   name: string;
-//   instrument: string;
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
 
-//   constructor(name: string, instrument: string) {
-//     this.name = name;
-//     this.instrument = instrument;
-//   }
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
 
-//   play(action: string): string {
-//     return `${this.name} ${action}`;
-//   }
-// }
+  play(action: string): string {
+    return `${this.name} ${action}`;
+  }
+}
 
-// let Page = new Guitarist("ali", "guitar");
-// console.log(Page.play("strums"));
+let Page = new Guitarist("ali", "guitar");
+console.log(Page.play("strums"));
 
-// class Peeps {
-//   static count: number = 0;
+class Peeps {
+  static count: number = 0;
 
-//   static getCount(): number {
-//     return Peeps.count;
-//   }
+  static getCount(): number {
+    return Peeps.count;
+  }
 
-//   public id: number;
+  public id: number;
 
-//   constructor(public name: string) {
-//     this.name = name;
-//     this.id = ++Peeps.count;
-//   }
-// }
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
 
-// const beyda = new Peeps("beyda");
-// const feyza = new Peeps("feyza");
+const beyda = new Peeps("beyda");
+const feyza = new Peeps("feyza");
 
-// console.log(Peeps.count);
-// console.log(feyza.id);
+console.log(Peeps.count);
+console.log(feyza.id);
 
 class Bands {
   private dataState: string[];
