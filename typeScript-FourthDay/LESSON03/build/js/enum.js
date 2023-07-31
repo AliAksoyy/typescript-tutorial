3e845640a438d516911d9d29e1081c7623fd8305
AliAksoyy <alifeyza4017@gmail.com>
2023-07-31 10:29:49 +0300

    aliases type

---
+++ typeScript-FourthDay/LESSON03/build/js/enum.js
@@ -0,0 +1,15 @@
+"use strict";
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade["C"] = "ali";
+    Grade[Grade["B"] = 7] = "B";
+    Grade[Grade["A"] = 8] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
+// interface Ali =stringOrNumberArray   //??'stringOrNumberArray' only refers to a type, but is being used as a value here
+// Literals types
+let myName;
+let userName;
+userName: "Amy";
---
+++ typeScript-FourthDay/LESSON03/src/enum.ts
@@ -0,0 +1,32 @@
+enum Grade {
+  U = 1,
+  D,
+  C = "ali",
+  B = 7,
+  A,
+}
+console.log(Grade.U);
+
+//?? Type Aliases
+
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = (string | number)[];
+
+type Guitar = {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+};
+
+type UserId = stringOrNumberArray;
+
+// interface Ali =stringOrNumberArray   //??'stringOrNumberArray' only refers to a type, but is being used as a value here
+
+// Literals types
+
+let myName: "Dave";
+
+let userName: "Dave" | "John" | "Amy";
+
+userName: "Amy";
