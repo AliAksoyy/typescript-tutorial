b6b222b0627c48d1ba1f0d5e1bb0b989dc7ce609
AliAksoyy <alifeyza4017@gmail.com>
2023-07-31 12:39:41 +0300

    added

--- typeScript-FourthDay/LESSON05/build/js/main.js
+++ typeScript-FourthDay/LESSON05/build/js/main.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("object");
+// convert to more or less spesific
+let a = "hello";
+let b = a;
+b = 4;
+b = "hello"; // les spesific
+let c = a; // more spesific
+let d = "world";
+let e = "world";
+const addOrConcat = (a, b, c) => {
+    if (c === "add") {
+        return a + b;
+    }
+    return "" + a + b;
+};
+let myVal = addOrConcat(2, 2, "concat");
+// ??Be careful TS sees no problem - but a string is returned
+let nextVal = addOrConcat(2, 2, "add");
+// 10 as string;
+10;
+// The DOM
+const img = document.querySelector("img"); // ?? HTMLImageElement | null
+const img1 = document.querySelector("#ali"); // ?? Element | null
+const myImg = document.getElementById("#img"); // ?? HTMLElement | null
+const nextImg = document.getElementById("#img");
+img.src;
+myImg.src;
--- typeScript-FourthDay/LESSON05/src/main.ts
+++ typeScript-FourthDay/LESSON05/src/main.ts
@@ -1,1 +1,47 @@
-console.log("object")
+type One = string;
+type Two = string | number;
+type Three = "hello";
+
+// convert to more or less spesific
+
+let a: One = "hello";
+let b = a as Two;
+b = 4;
+b = "hello"; // les spesific
+let c = a as Three; // more spesific
+
+let d = <One>"world";
+let e = <string | number>"world";
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") {
+    return a + b;
+  }
+  return "" + a + b;
+};
+
+let myVal: string = addOrConcat(2, 2, "concat") as string;
+
+// ??Be careful TS sees no problem - but a string is returned
+
+let nextVal: number = addOrConcat(2, 2, "add") as number;
+
+// 10 as string;
+10 as unknown as string;
+
+// The DOM
+
+const img = document.querySelector("img") as HTMLImageElement; // ?? HTMLImageElement | null
+
+const img1 = document.querySelector("#ali"); // ?? Element | null
+
+const myImg = document.getElementById("#img")! as HTMLImageElement; // ?? HTMLElement | null
+
+const nextImg = <HTMLImageElement>document.getElementById("#img");
+
+img.src;
+myImg.src;
