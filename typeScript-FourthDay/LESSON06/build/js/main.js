d2e2744ebc3aaaef94b698ee23106c78c35dfd6e
AliAksoyy <alifeyza4017@gmail.com>
2023-07-31 14:19:37 +0300

    added interface

--- typeScript-FourthDay/LESSON06/build/js/main.js
+++ typeScript-FourthDay/LESSON06/build/js/main.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("object");
+// class Coder {
+//   name: string;
+//   music: string;
+//   age: number;
+//   lang: String;
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action}`;
+    }
+}
+let Page = new Guitarist("ali", "guitar");
+console.log(Page.play("strums"));
--- typeScript-FourthDay/LESSON06/src/main.ts
+++ typeScript-FourthDay/LESSON06/src/main.ts
@@ -1,1 +1,80 @@
-console.log("object")
+// class Coder {
+//   name: string;
+//   music: string;
+//   age: number;
+//   lang: String;
+
+//   constructor(name: string, music: string, age: number, lang: string) {
+//     this.name = name;
+//     this.music = music;
+//     this.age = age;
+//     this.lang = lang;
+//   }
+// }
+// class Coder1 {
+//   secondLang!: string;
+
+//   constructor(
+//     public readonly name: string,
+//     public music: string,
+//     private age: number,
+//     protected lang: string = "typescript"
+//   ) {
+//     this.name = name;
+//     this.music = music;
+//     this.age = age;
+//     this.lang = lang;
+//   }
+
+//   public getAge() {
+//     return `Hello I'm ${this.age}`;
+//   }
+// }
+
+// let ali = new Coder1("ali", "neset", 3, "turkish");
+// console.log(ali);
+// console.log(ali.getAge());
+// // console.log(ali.age); //?Property 'age' is private and only accessible within class 'Coder1'
+// // console.log(ali.lang); // ?? Property 'lang' is protected and only accessible within class 'Coder1' and its subclasses
+
+// class WebDev extends Coder1 {
+//   constructor(
+//     public computer: string,
+//     name: string,
+//     music: string,
+//     age: number
+//   ) {
+//     super(name, music, age);
+//     this.computer = computer;
+//   }
+
+//   public getLang() {
+//     return `I write ${this.lang}`;
+//   }
+// }
+
+// const Sara = new WebDev("Mac","Sarah","Lofi",25);
+// console.log(Sara)
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play(action: string): string {
+    return `${this.name} ${action}`;
+  }
+}
+
+let Page = new Guitarist("ali", "guitar");
+console.log(Page.play("strums"));
