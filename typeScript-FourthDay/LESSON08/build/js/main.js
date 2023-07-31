2b8a4d538aa3a2f16f9cc1f1578cb0e9c4e2fdf1
AliAksoyy <alifeyza4017@gmail.com>
2023-07-31 17:31:53 +0300

    added

--- typeScript-FourthDay/LESSON08/build/js/main.js
+++ typeScript-FourthDay/LESSON08/build/js/main.js
@@ -5,10 +5,30 @@ const stringEcho = (arg) => {
 const stringEcho1 = (arg) => {
     return arg;
 };
-console.log(stringEcho("arg"));
+// console.log(stringEcho("arg"));
 const isObj = (arg) => {
     return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
 console.log(isObj("arg"));
 console.log(isObj(["arg"]));
 console.log(isObj({ a: 2 }));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return {
+            arg,
+            is: false,
+        };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return {
+        arg,
+        is: !!arg,
+    };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue({}));
+console.log(isTrue(NaN));
--- typeScript-FourthDay/LESSON08/src/main.ts
+++ typeScript-FourthDay/LESSON08/src/main.ts
@@ -5,7 +5,7 @@ const stringEcho1 = <T>(arg: T): T => {
   return arg;
 };
 
-console.log(stringEcho("arg"));
+// console.log(stringEcho("arg"));
 
 const isObj = <T>(arg: T): boolean => {
   return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
@@ -14,3 +14,25 @@ const isObj = <T>(arg: T): boolean => {
 console.log(isObj("arg"));
 console.log(isObj(["arg"]));
 console.log(isObj({ a: 2 }));
+
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return {
+      arg,
+      is: false,
+    };
+  }
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return {
+    arg,
+    is: !!arg,
+  };
+};
+
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue({}));
+console.log(isTrue(NaN));
