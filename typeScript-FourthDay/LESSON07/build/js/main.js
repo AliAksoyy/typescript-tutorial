67975b8eba3b1d2bdbe31fcd8b75ee9c62c783ec
AliAksoyy <alifeyza4017@gmail.com>
2023-07-31 15:23:37 +0300

    added signatures

--- typeScript-FourthDay/LESSON07/build/js/main.js
+++ typeScript-FourthDay/LESSON07/build/js/main.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("object");
+// Index Signatures
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+    Ali: 43
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Books"]);
+let prop = "Pizza";
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
+// todaysTransactions.Pizza = 40; //? Index signature in type 'TransactionObj' only permits reading.
+console.log(todaysTransactions["Ali"]);
--- typeScript-FourthDay/LESSON07/src/main.ts
+++ typeScript-FourthDay/LESSON07/src/main.ts
@@ -1,1 +1,37 @@
-console.log("object")
+// Index Signatures
+
+// interface TransactionObj {
+//   readonly [index: string]: number;
+// }
+interface TransactionObj {
+  readonly [index: string]: number;
+  Pizza: number;
+  Books: number;
+  Job: number;
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+  Ali: 43
+};
+
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Books"]);
+
+let prop: string = "Pizza";
+console.log(todaysTransactions[prop]);
+
+const todaysNet = (transactions: TransactionObj): number => {
+  let total = 0;
+  for (const transaction in transactions) {
+    total += transactions[transaction];
+  }
+  return total;
+};
+
+console.log(todaysNet(todaysTransactions));
+// todaysTransactions.Pizza = 40; //? Index signature in type 'TransactionObj' only permits reading.
+
+console.log(todaysTransactions["Ali"]);
