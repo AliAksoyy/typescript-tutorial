401ee1391950c47b1b7e3e806324c2516010c8a5
AliAksoyy <alifeyza4017@gmail.com>
2023-08-01 10:54:41 +0300

    added

--- typeScript-FourthDay/LESSON09/src/main.ts
+++ typeScript-FourthDay/LESSON09/src/main.ts
@@ -88,3 +88,60 @@ const preview: AssignPreview = {
   studentId: "k123",
   title: "Final Project",
 };
+
+// Exclude and Extract
+
+type adjustGrade = Exclude<LetterGrades, "U">;
+
+type highGrade = Extract<LetterGrades, "A" | "B">;
+
+// NonNullable
+
+type AllPossibleGrades = "Dave" | "Ali" | "Beyda" | null | undefined;
+
+type NamesOnly = NonNullable<AllPossibleGrades>;
+
+// ReturnType
+
+const createNewAssign = (title: string, points: number) => {
+  return { title, points };
+};
+
+type NewAssign = ReturnType<typeof createNewAssign>;
+
+const tsAssign: NewAssign = createNewAssign("Utility Types", 100);
+
+console.log(tsAssign);
+
+// Parameters
+
+type AsignParams = Parameters<typeof createNewAssign>;
+
+const assignArgs: AsignParams = ["Generics", 100];
+
+const tsAssign2: NewAssign = createNewAssign(...assignArgs);
+console.log(tsAssign2);
+
+//Awaited - Hepls us with ReturnType of a Promise
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then((res) => res.json())
+    .catch((err) => {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
+    });
+  return data;
+};
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+
+fetchUsers().then((users)=>console.log(users))
