580ebc70619b573d26318213a731fa54df098fc0
AliAksoyy <alifeyza4017@gmail.com>
2023-08-01 06:34:26 +0300

    added

--- typeScript-FourthDay/LESSON08/src/main.ts
+++ typeScript-FourthDay/LESSON08/src/main.ts
@@ -2,21 +2,26 @@ const stringEcho = (arg: string): string => {
   return arg;
 };
 
-console.log(stringEcho("arg"));
+// console.log(stringEcho("arg"));
 
 const stringEcho1 = <T>(arg: T): T => {
   return arg;
 };
 
-console.log(stringEcho1("arg"));
-console.log(stringEcho1(4));
+// console.log(stringEcho1("arg"));
+// console.log(stringEcho1(4));
 
 const isObj = <T>(arg: T): boolean => {
   return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
-console.log(isObj(true));
-console.log(isObj({ a: 4 }));
-console.log(isObj([{ a: 4 }]));
+// console.log(isObj(true));
+// console.log(isObj({ a: 4 }));
+// console.log(isObj([{ a: 4 }]));
+
+interface BoolCheck<T> {
+  value: T;
+  is: boolean;
+}
 
 const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
   if (Array.isArray(arg) && !arg.length) {
@@ -29,18 +34,39 @@ const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
   return { arg, is: !!arg };
 };
 
+//??????????
+const isTrue1 = <T>(arg: T): BoolCheck<T> => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { value: arg, is: false };
+  }
 
-console.log(isTrue(false));
-console.log(isTrue(0));
-console.log(isTrue(1));
-console.log(isTrue("ali"));
-console.log(isTrue(""));
-console.log(isTrue(null));
-console.log("a",isTrue({}));
-console.log(isTrue(NaN));
-console.log(isTrue(-0));
-console.log("b",isTrue([]));
-console.log(isTrue([1,2,3]));
-console.log(isTrue({ a: 4 }));
-console.log(isTrue([{ a: 4 }]));
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { value: arg, is: false };
+  }
+  return { value: arg, is: !!arg };
+};
 
+// console.log(isTrue(false));
+// console.log(isTrue(0));
+// console.log(isTrue(1));
+// console.log(isTrue("ali"));
+// console.log(isTrue(""));
+// console.log(isTrue(null));
+// console.log("a", isTrue({}));
+// console.log(isTrue(NaN));
+// console.log(isTrue(-0));
+// console.log("b", isTrue([]));
+// console.log(isTrue([1, 2, 3]));
+// console.log(isTrue({ a: 4 }));
+// console.log(isTrue([{ a: 4 }]));
+
+interface HasID {
+  id: number;
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+  // process the user with logic here
+  return user;
+};
+
+console.log(processUser({ id: 3, name: "ali" }));
