92e37d853c0daa47e2c5685d199139ad8f632ad9
AliAksoyy <alifeyza4017@gmail.com>
2023-08-01 06:02:43 +0300

    generic

--- typeScript-FourthDay/LESSON08/build/js/main.js
+++ typeScript-FourthDay/LESSON08/build/js/main.js
@@ -32,3 +32,25 @@ console.log(isTrue(0));
 console.log(isTrue(true));
 console.log(isTrue({}));
 console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: true };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return {
+        value: arg,
+        is: !!arg,
+    };
+};
+const processUser = (user) => {
+    // process the user with logic here
+    return user;
+};
+console.log(processUser({ id: 1, name: "ali" }));
+const getUsersProperty = (users, key) => {
+    return [users, key];
+};
+console.log(getUsersProperty({ name: "ali", id: 2 }, "ali"));
