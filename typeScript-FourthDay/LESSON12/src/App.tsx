da408d8dc5cd806072fe46d35e347e436146adb9
AliAksoyy <alifeyza4017@gmail.com>
2023-08-02 16:48:47 +0300

    added

---
+++ typeScript-FourthDay/LESSON12/src/App.tsx
@@ -0,0 +1,12 @@
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+const App = () => {
+  return (
+    <>
+      <Heading title={"Hello"} />
+      <Section>This is my section</Section>
+    </>
+  );
+};
+
+export default App;
