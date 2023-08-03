1cb4e800e623252344dfef58d86f0f6176f8026d
AliAksoyy <alifeyza4017@gmail.com>
2023-08-03 07:18:38 +0300

    added

---
+++ typeScript-FourthDay/LESSON14/src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+
+
+
+const App = () => {
+  return (
+    <>
+    <Counter>{(num:number)=><>Current Count: {num}</>}</Counter>
+    </>
+  )
+};
+
+export default App;
