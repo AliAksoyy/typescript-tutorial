78880309f87e301a12e8c4c67fb88d69b3b0c7df
AliAksoyy <alifeyza4017@gmail.com>
2023-08-06 06:12:17 +0300

    added

--- typeScript-FifthDay/LESSON04/src/cl-interfaces-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON04/src/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,30 +1,10 @@
-class Department {
-  public name: string;
-  private employees: string[] = [];
+// type AddFn = (a: string) => string;
 
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  describe(this: Department) {
-    console.log("Department" + this.name);
-  }
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+interface AddFn {
+  (a:string):string
 }
 
-let accounting = new Department("ali");
-
-accounting.describe();
-accounting.addEmployee("3")
-accounting.employees.push("feyza", "fr");
-
-console.log(accounting);
+let sum: AddFn = (a: string) => {
+  return a;
+};
 
