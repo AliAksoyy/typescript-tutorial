bf2715bf284bc3e37168fc68da7c18af3d3a38c7
AliAksoyy <alifeyza4017@gmail.com>
2023-08-06 07:03:37 +0300

    add

--- typeScript-FifthDay/LESSON05/adv-types-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON05/adv-types-01-starting-setup/src/app.ts
@@ -1,1 +1,25 @@
-console.log("objssssect")
+type Bird = {
+  type: "bird";
+  flySpeed: number;
+};
+
+type Horse = {
+  type: "horse";
+  runSpeed: number;
+};
+
+type Animal = Bird | Horse;
+
+function moveAnimal(item: Animal) {
+  let speed;
+  switch (item.type) {
+    case "bird":
+      speed = item.flySpeed;
+      break;
+    case "horse":
+      speed = item.runSpeed;
+  }
+  console.log(speed);
+}
+
+moveAnimal({type:"bird", flySpeed:3})
