74145437285192cf1f3d4915fa66c2a93b633898
AliAksoyy <alifeyza4017@gmail.com>
2023-08-07 11:49:59 +0300

    added decorator

--- typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
@@ -1,1 +1,14 @@
-console.log("s")
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+@Logger("LOGGING - PERSON")
+class Person {
+  name: string = "Ali";
+  constructor() {
+    console.log("Creating person object");
+  }
+}
