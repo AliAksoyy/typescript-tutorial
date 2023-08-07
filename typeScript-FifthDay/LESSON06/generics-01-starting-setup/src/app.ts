d88a0ae966a869a7cef9af86fdfedb54904cfbb2
AliAksoyy <alifeyza4017@gmail.com>
2023-08-07 07:13:14 +0300

    added

--- typeScript-FifthDay/LESSON06/generics-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON06/generics-01-starting-setup/src/app.ts
@@ -1,2 +1,6 @@
-// Code goes here!
-console.log("object")
+function mege(objA: Object, objB: Object): Object {
+  return Object.assign(objA, objB);
+}
+
+let a = mege({ name: "s" }, { age: 30 }) as { name: string; age: number };
+
