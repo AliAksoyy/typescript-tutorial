fcb901f42c224bb0a663c34f53107e85d29f5c42
AliAksoyy <alifeyza4017@gmail.com>
2023-08-07 17:18:30 +0300

    added

--- typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
@@ -1,48 +1,36 @@
-function Log(target: any, propertyName: string) {
-  console.log("... Logged");
+function Autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
   console.log(target);
-  console.log(propertyName);
-}
-
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Accessor decarotor");
-  console.log(target);
-  console.log(name);
+  console.log(methodName);
   console.log(descriptor);
+  console.log(descriptor.value);
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
 
-function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Method decarotor");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
+class Print {
+  msg: string = "Hello";
 
-function Log4(target: any, name: string, position: number) {
-  console.log("Parameter decarotor");
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
-
-class Person {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val) {
-      this._price = val;
-    }
-  }
-
-  constructor(title: string, price: number) {
-    this.title = title;
-    this._price = price;
-  }
-  @Log3
-  getPrice(@Log4 tax: number) {
-    this._price * (1 + tax);
+  constructor() {}
+  @Autobind
+  showMsg() {
+    console.log(this.msg);
   }
 }
+
+let msg = new Print();
+
+let button = document.querySelector("button")!;
+
+button?.addEventListener("click", msg.showMsg);
