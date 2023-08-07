c72ed9b2c828b9b4c3327d793b3eee6895fb7d7b
AliAksoyy <alifeyza4017@gmail.com>
2023-08-07 14:21:06 +0300

    adde

--- typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
@@ -1,23 +1,52 @@
-function Logger(logString: string) {
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
+// function Logger(logString: string) {
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+// function WithTemplate(template: string, hookId: string) {
+//   return function (constructor: any) {
+//     let hookData = document.getElementById(hookId);
+//     const p = new constructor()
+//     if (hookData) {
+//       (hookData as HTMLDivElement).innerHTML = template;
+//         hookData.querySelector("h1")!.textContent = p.name
+
+//     }
+//   };
+// }
+// @WithTemplate("<h1>My person Object</h1>", "app")
+// @Logger("LOGGING - PERSON")
+// class Person {
+//   name: string = "Ali";
+//   constructor() {
+//     console.log("Creating person object");
+//   }
+// }
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log("Property Decaroter")
+    console.log(target, propertyName);
 }
 
-function WithA(template: string, hookId: string) {
-  return function (_constructor: Function) {
-    let hookData = document.getElementById(hookId);
-    if (hookData) {
-      (hookData as HTMLDivElement).innerHTML = template;
+class Product {
+  @Log
+  title: string;
+  price: number;
+
+  set setPrice(val: number) {
+    if (val) {
+      this.price = val;
+    } else {
+      throw new Error("Product");
     }
-  };
-}
-@WithA("ssss","app")
-@Logger("LOGGING - PERSON")
-class Person {
-  name: string = "Ali";
-  constructor() {
-    console.log("Creating person object");
+  }
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+  getPrice(tax: number) {
+    return this.price * (1 + tax);
   }
 }
