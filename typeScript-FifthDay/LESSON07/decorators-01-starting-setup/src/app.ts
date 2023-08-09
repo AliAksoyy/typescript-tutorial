d4934914ee535e024f24275d6d581d1cdbd4f1a0
AliAksoyy <alifeyza4017@gmail.com>
2023-08-09 10:12:36 +0300

    added

--- typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON07/decorators-01-starting-setup/src/app.ts
@@ -1,98 +1,23 @@
-interface ValidatorConfig {
-  [property: string]: {
-    [validatableProp: string]: string[];
-  };
-}
+// function merge<T, U>(a: T, b: U): T & U {
+//   return { ...a, ...b };
+// }
 
-const registeredValidators: ValidatorConfig = {};
-function Required(target: any, propName: string) {
-  console.log(target);
-  console.log(propName);
-  registeredValidators[target.constructor.name] = {
-    ...registeredValidators[target.constructor.name],
-    [propName]: ["required"],
-  };
-console.log(registeredValidators);
-
-}
-function PositiveNumber(target: any, propName: string) {
-  console.log(target);
-  console.log(propName);
-  registeredValidators[target.constructor.name] = {
-    ...registeredValidators[target.constructor.name],
-    [propName]: ["positive"],
-  };
-}
-
-function validate(obj: any) {
-  console.log("aaaa", obj.constructor.name);
-  const objectValidatorConfig = registeredValidators[obj.constructor.name];
-  console.log(objectValidatorConfig);
-  if (!objectValidatorConfig) {
-    return true;
-  }
-  let isValid = true;
-  for (const prop in objectValidatorConfig) {
-    console.log(prop)
-    for (const validator of objectValidatorConfig[prop]) {
-      switch (validator) {
-        case "required":
-          isValid = isValid && !!obj[prop];
-          break;
-        case "positive":
-          isValid = isValid && obj[prop] > 0;
-          break;
-      }
-    }
-  }
-  return isValid;
-}
-
-class Course {
-  @Required
-  title: string;
-  @PositiveNumber
-  price: number;
-
-  constructor(t: string, p: number) {
-    this.title = t;
-    this.price = p;
-  }
-}
-
-const courseForm = <HTMLFormElement>document.querySelector("form");
-
-if (courseForm) {
-  courseForm.addEventListener("submit", (event: SubmitEvent) => {
-    event.preventDefault();
-    const titleEl = document.querySelector("#title") as HTMLInputElement;
-    const priceEl = document.querySelector("#price") as HTMLInputElement;
-
-    const title = titleEl.value;
-    const price = Number(priceEl.value);
-
-    let createdCourse = new Course(title, price);
-    if (!validate(createdCourse)) {
-      alert("Invalid input please try again");
-      return;
-    }
-
-    console.log(createdCourse);
-  });
-}
-
-
-// const salary = 1000000;
-// const a = Object.assign(
-//   [...salary.toString()],
-//   {
-//     salary,
-//   },
-//   {
-//     0: [..."emre"],
-//     1: [Object.assign([..."can"], { 3: "terzi" })],
-//     2: [..."terzi"],
-//   }
+// let obj = merge<{ name: string; hobi: Array<string> }, { age: number }>(
+//   { name: "ali", hobi: ["2"] },
+//   { age: 2 }
 // );
+// console.log(obj);
 
-// console.log(a);
+let a = 1000;
+let b = "ali";
+let d = Object.assign(
+  [...a.toString()],
+  { 9: a },
+  { d: Object.assign([...b], { 3: 2 }) }
+);
+console.log(d);
+
+d.map((a, i) => {
+  console.log("a", a);
+  console.log("index", i);
+});
