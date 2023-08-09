25206464211e5981902735cbc2db31946efdf335
AliAksoyy <alifeyza4017@gmail.com>
2023-08-09 13:56:58 +0300

    added

--- typeScript-FifthDay/LESSON08/prj-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON08/prj-01-starting-setup/src/app.ts
@@ -1,1 +1,21 @@
-console.log("object")
+class Input {
+  template: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  el: HTMLFormElement;
+  constructor() {
+    this.template = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostEl = document.getElementById("app")! as HTMLDivElement;
+
+    const importNode = document.importNode(this.template.content, true);
+    this.el = importNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.el);
+  }
+}
+
+let a = new Input();
