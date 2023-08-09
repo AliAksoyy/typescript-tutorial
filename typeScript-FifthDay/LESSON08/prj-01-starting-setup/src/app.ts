f79f6e3dd383ca8c7802c54cea2c5c89c2868f3a
AliAksoyy <alifeyza4017@gmail.com>
2023-08-09 14:17:53 +0300

    added

--- typeScript-FifthDay/LESSON08/prj-01-starting-setup/src/app.ts
+++ typeScript-FifthDay/LESSON08/prj-01-starting-setup/src/app.ts
@@ -1,20 +1,49 @@
 class Input {
-  template: HTMLTemplateElement;
+  templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
-  el: HTMLFormElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
-    this.template = document.getElementById(
+    this.templateEl = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
     this.hostEl = document.getElementById("app")! as HTMLDivElement;
 
-    const importNode = document.importNode(this.template.content, true);
-    this.el = importNode.firstElementChild as HTMLFormElement;
+    const importNode = document.importNode(this.templateEl.content, true);
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    //   const titleValue = this.titleInputElement.value;
+    //   const DescriptionValue = this.descriptionInputElement.value;
+    //   const peopleValue = this.peopleInputElement.value;
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
   private attach() {
-    this.hostEl.insertAdjacentElement("afterbegin", this.el);
+    this.hostEl.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
