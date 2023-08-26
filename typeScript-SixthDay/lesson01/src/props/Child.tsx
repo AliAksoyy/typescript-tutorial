a47fd546006adccb357feec0af4526e68ab62397
AliAksoyy <alifeyza4017@gmail.com>
2023-08-26 15:32:06 +0300

    add

---
+++ typeScript-SixthDay/lesson01/src/props/Child.tsx
@@ -0,0 +1,20 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+  children?: React.ReactNode;
+}
+
+export const ChildAsFC: React.FunctionComponent<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      {color}
+      {children}
+      <input type="text" />
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+};
