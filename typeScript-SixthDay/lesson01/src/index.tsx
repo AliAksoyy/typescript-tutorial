005ae607c501111416fa4c4dcc3999e627f8e159
AliAksoyy <alifeyza4017@gmail.com>
2023-08-28 09:40:11 +0300

    add

--- typeScript-SixthDay/lesson01/src/index.tsx
+++ typeScript-SixthDay/lesson01/src/index.tsx
@@ -2,13 +2,14 @@ import { createRoot } from "react-dom/client";
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
 import EventComponent from "./events/EventComponent";
+import Parent from "./props/Parent";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-const App = () => {
+const App= () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <Parent />
       <GuestList />
       <UserSearch />
       <EventComponent />
